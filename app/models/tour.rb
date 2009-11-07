c70cce34e4ac0e359121130f0c488d2cd4594fe7
Steve Martocci <smartocci@gilt.com>
2009-11-07 18:30:08 -0500

    validate the uniqueness of pt id for tours

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,6 +2,7 @@ class Tour < ActiveRecord::Base
   has_many :shows
 
   validates_presence_of :name
+  validates_uniqueness_of :pt_id
 
   def self.find_or_create_by_pt_id(id)
     pt_data = PhantasyTour.new.tour_info_from_id(id)
