e6ee1dee34476c5e68ea7e0c6255ffc57db63bd0
Steve Martocci <smartocci@gilt.com>
2009-11-27 00:53:20 -0500

    system controler

--- app/controllers/system_controller.rb
+++ app/controllers/system_controller.rb
@@ -2,7 +2,7 @@ class SystemController < ApplicationController
 
 
   def request_info
-    render :template => "system/request_info.html.haml", :layout => false
+    render :template => "system/request_info.html.haml"
   end
 
 end
